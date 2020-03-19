2cc64f9cf52583d14a909c8d71f4763cd75e34b8
Bartosz Ornoch <bartoszornoch@pop-os.localdomain>
2020-03-19 22:59:49 +0100

    dodano text, wyswietlanie nastepnego klocka

--- main.py
+++ main.py
@@ -5,116 +5,133 @@ import random
 from tetrion import Tetrion
 
 
-def square_postion(x, y, local_shift_x, local_shift_y):
-    return ((x+local_shift_x)*square_size + GLOBAL_SHIFT_X, (y + local_shift_y)*square_size + GLOBAL_SHIFT_Y, square_size, square_size)
+def square_coords(x, y, local_shift_x, local_shift_y):
+    return ((x+local_shift_x)*SQUARE_SIZE + GLOBAL_SHIFT_X, (y + local_shift_y)*SQUARE_SIZE + GLOBAL_SHIFT_Y, SQUARE_SIZE, SQUARE_SIZE)
 
 
 def print_white_background():
-    screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
 
 
-def print_board(array2d, local_shift_x=0, local_shift_y=0):
+def draw_squares(array2d, local_shift_x=0, local_shift_y=0):
     for y, row in enumerate(array2d):
         for x, cell in enumerate(row):
             if cell:
                 pygame.draw.rect(
-                    screen, colors[cell], square_postion(x, y, local_shift_x, local_shift_y))
+                    screen, COLORS[cell], square_coords(x, y, local_shift_x, local_shift_y))
 
 
 def get_random_block():
     return random.choice([blocks.BlockI(), blocks.BlockJ(), blocks.BlockL(), blocks.BlockO(), blocks.BlockS(), blocks.BlockT(), blocks.BlockZ()])
 
 
-colors = [(255, 255, 255), (255, 255, 255), (0, 255, 255), (128, 0, 128),
+def draw_text(text, postion_x, postion_y):
+    screen.blit(font.render(
+        text, True, COLORS[0]), (postion_x*SQUARE_SIZE, postion_y*SQUARE_SIZE))
+
+
+COLORS = [(255, 255, 255), (0, 0, 0), (0, 255, 255), (128, 0, 128),
           (255, 165, 0), (0, 0, 255), (0, 128, 0), (255, 0, 0), (255, 255, 0)]
 
-square_size = 40
-GLOBAL_SHIFT_X = - square_size
-GLOBAL_SHIFT_Y = - square_size*4
-
+SQUARE_SIZE = 40
+GLOBAL_SHIFT_X = - SQUARE_SIZE
+GLOBAL_SHIFT_Y = - SQUARE_SIZE*4
+WIDTH = SQUARE_SIZE * 21
+HEIGHT = SQUARE_SIZE * 22
 
 pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+font = pygame.font.SysFont("comicsansms", 40)
 
-rows = 22
-cols = 14
 
-screen = pygame.display.set_mode((cols*square_size, rows*square_size))
+def main():
 
-running = True
-time = 0
-score = 0
-prepare_next = False
-tetrion = Tetrion()
-block = get_random_block()
-next_block = get_random_block()
+    running = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    prepare_next = False
+    tetrion = Tetrion()
+    block = get_random_block()
+    next_block = get_random_block()
 
-        keys = pygame.key.get_pressed()
+    time = 0
+    score = 0
+    level = 1
 
-        if keys[pygame.K_d]:
-            if block.check_if_on_edge('right'):
-                x, y = block.get_coords()
-                part = tetrion.get_part(x+1, y)
-                if block.can_move(part):
-                    block.move_right()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        if keys[pygame.K_a]:
-            if block.check_if_on_edge('left'):
-                x, y = block.get_coords()
-                part = tetrion.get_part(x-1, y)
-                if block.can_move(part):
-                    block.move_left()
+            keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_r]:
-            x, y = block.get_coords()
-            part = tetrion.get_part(x, y)
-            if block.can_rotate(part):
-                print('nic')
-                block.rotate()
-
-        if keys[pygame.K_s]:
-            while prepare_next == False:
-                if block.check_if_on_edge('bottom'):
+            if keys[pygame.K_d]:
+                if block.check_if_on_edge('right'):
                     x, y = block.get_coords()
-                    part = tetrion.get_part(x, y+1)
+                    part = tetrion.get_part(x+1, y)
                     if block.can_move(part):
-                        block.move_down()
+                        block.move_right()
+
+            if keys[pygame.K_a]:
+                if block.check_if_on_edge('left'):
+                    x, y = block.get_coords()
+                    part = tetrion.get_part(x-1, y)
+                    if block.can_move(part):
+                        block.move_left()
+
+            if keys[pygame.K_r]:
+                x, y = block.get_coords()
+                part = tetrion.get_part(x, y)
+                if block.can_rotate(part):
+                    print('nic')
+                    block.rotate()
+
+            if keys[pygame.K_s]:
+                while prepare_next == False:
+                    if block.check_if_on_edge('bottom'):
+                        x, y = block.get_coords()
+                        part = tetrion.get_part(x, y+1)
+                        if block.can_move(part):
+                            block.move_down()
+                        else:
+                            prepare_next = True
                     else:
                         prepare_next = True
+
+        if (pygame.time.get_ticks() // 500) > time:
+
+            if block.check_if_on_edge('bottom'):
+                x, y = block.get_coords()
+                part = tetrion.get_part(x, y+1)
+                if block.can_move(part):
+                    block.move_down()
                 else:
                     prepare_next = True
-
-    if (pygame.time.get_ticks() // 500) > time:
-
-        if block.check_if_on_edge('bottom'):
-            x, y = block.get_coords()
-            part = tetrion.get_part(x, y+1)
-            if block.can_move(part):
-                block.move_down()
             else:
                 prepare_next = True
-        else:
-            prepare_next = True
-        time += 1
-        print(f'minelo {time} sekund')
+            time += 0.5
+            print(f'minelo {time} sekund')
 
-    if prepare_next:
-        tetrion.update_board(block.state, block.y, block.x)
-        score += tetrion.remove_rows() ** 2 * 1000
-        print(score)
-        block = next_block
-        next_block = get_random_block()
-        prepare_next = False
+        if prepare_next:
+            tetrion.update_board(block.state, block.y, block.x)
+            score += tetrion.remove_rows() ** 2 * 1000
+            block = next_block
+            next_block = get_random_block()
+            prepare_next = False
 
-    print_white_background()
-    print_board(tetrion.board)
-    print_board(block.state, block.x, block.y)
-    pygame.draw.rect(screen, (0, 0, 0),
-                     (square_size, square_size, square_size*10, square_size*20), 1)
-    # print_block(next_block)
+        print_white_background()
+        draw_squares(tetrion.board)
+        draw_squares(block.state, block.x, block.y)
+        draw_squares(next_block.state, 17, 5)
+        pygame.draw.rect(screen, (255, 255, 255),
+                         (SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE*10, SQUARE_SIZE*20), 2)
+        draw_text('NEXT BLOCK:', 12, 4)
+        draw_text(f'SCORE: {score}', 12, 8)
+        draw_text(f'LEVEL: {level}', 12, 9)
+        draw_text(f'TIME: {time}', 12, 10)
+        # print_block(next_block)
 
-    pygame.display.update()
+        pygame.display.update()
+
+
+main()
